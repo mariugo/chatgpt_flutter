a722fbc304d1230d99a6d559e27c63ad373f1ea6
Mariugo Megiani <mariugomegiani@gmail.com>
2023-02-09 17:09:21 -0300

    Assets constants impl

---
+++ lib/core/constants/assets_constants.dart
@@ -0,0 +1,4 @@
+const String imagePath = 'assets/images';
+const String userImage = '$imagePath/person.png';
+const String botImage = '$imagePath/chat_logo.png';
+const String openAiLogo = '$imagePath/openai_logo.jpg';
