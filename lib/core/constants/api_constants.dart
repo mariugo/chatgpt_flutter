db4dc93edb608d9217997d188b2b808e75d25519
Mariugo Megiani <mariugomegiani@gmail.com>
2023-02-09 15:20:09 -0300

    Constants impl

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,2 @@
+const baseUrl = 'https://api.openai.com/v1';
+const apiKey = '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';
---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const primaryColor = Color(0xFF343541);
+  static const cardColor = Color(0xFF444654);
+}
